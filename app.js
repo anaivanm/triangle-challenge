3ce304ce276c21583f851bd038ef1d5abcf8b993
Ana Ivan <ana.ivan.m@gmail.com>
2019-11-10 15:19:33 +0100

    Additional tweaks - ui and js refacto

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ triangles.forEach(function(triangle) {
     const borderRight = triangleStyle.borderRight.split(" ")[0]
     const borderBottom = triangleStyle.borderBottom.split(" ")[0]
     if (allSidesEqual(borderLeft, borderRight, borderBottom)) {
-      triangle.nextElementSibling.innerText = 'I am an equilateral triangle.'
+      showResult(triangle.nextElementSibling, 'I am an equilateral triangle.')
+      // triangle.nextElementSibling.innerText = 'I am an equilateral triangle.'
     } else if (twoSidesEqual(borderLeft, borderRight, borderBottom)) {
-      triangle.nextElementSibling.innerText = 'I am an isosceles triangle.'
+      showResult(triangle.nextElementSibling, 'I am an isosceles triangle.')
+      // triangle.nextElementSibling.innerText = 'I am an isosceles triangle.'
     } else {
-      triangle.nextElementSibling.innerText = 'I am neither.'
+      showResult(triangle.nextElementSibling, 'I am a scalene triangle.')
+      // triangle.nextElementSibling.innerText = 'I am a scalene triangle.'
     }
   })
 })
@@ -24,4 +27,12 @@ function allSidesEqual(side1, side2, side3) {
 
 function twoSidesEqual(side1, side2, side3) {
   return side1 === side2 && side2 !== side3 && side1 !== side3
+}
+
+function showResult(result, string) {
+  if (!result.innerText) {
+    return result.innerText = string
+  } else {
+    return result.innerText = ''
+  }
 }
