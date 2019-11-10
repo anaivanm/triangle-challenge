6f2da5e5a476e6d3b867c98a7e8bea5f1a294acf
Ana Ivan <ana.ivan.m@gmail.com>
2019-11-10 16:42:55 +0100

    Remove comments

--- app.js
+++ app.js
@@ -3,20 +3,16 @@ const triangles = document.querySelectorAll('.triangle')
 triangles.forEach(function(triangle) {
   triangle.addEventListener('click', function(e) {
     e.stopPropagation()
-    // console.log(this.classList.value)
     const triangleStyle = window.getComputedStyle(triangle)
     const borderLeft = triangleStyle.borderLeft.split(" ")[0]
     const borderRight = triangleStyle.borderRight.split(" ")[0]
     const borderBottom = triangleStyle.borderBottom.split(" ")[0]
     if (allSidesEqual(borderLeft, borderRight, borderBottom)) {
       showResult(triangle.nextElementSibling, 'I am an equilateral triangle.')
-      // triangle.nextElementSibling.innerText = 'I am an equilateral triangle.'
     } else if (twoSidesEqual(borderLeft, borderRight, borderBottom)) {
       showResult(triangle.nextElementSibling, 'I am an isosceles triangle.')
-      // triangle.nextElementSibling.innerText = 'I am an isosceles triangle.'
     } else {
       showResult(triangle.nextElementSibling, 'I am a scalene triangle.')
-      // triangle.nextElementSibling.innerText = 'I am a scalene triangle.'
     }
   })
 })
